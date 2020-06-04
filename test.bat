1e4acbb7560b2425f7204a8fcecce6f95bea5b9d
howarder3 <howarder3@gmail.com>
2020-06-04 15:27:28 +0800

    2020/06/04-15:27:27

--- test.bat
+++ test.bat
@@ -1,9 +1,26 @@
 @ECHO OFF
 ECHO Congratulations! Your first batch file executed successfully.
 
-set /p msg="Enter msg: "
+for /f "tokens=1-4 delims=-/ " %%i IN ("%date%") DO (
+set year=%%i
+set month=%%j
+set day=%%k
+)
+
+for /f "tokens=1-4 delims=:." %%i IN ("%time%") DO (
+set hour=%%i
+set minute=%%j
+set second=%%k
+rem set centisecond=%%l
+)
+
+SET OutputDir=%year%/%month%/%day%-%hour%:%minute%:%second%
+ECHO %OutputDir%
+
+rem set /p msg="Enter msg: "
 git add .
-git commit -m msg
+rem git commit -m msg
+git commit -m %OutputDir%
 git push origin master
 
 PAUSE
