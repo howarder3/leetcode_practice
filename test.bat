762ec738a0cadf371e63f05cee43ac267070eec9
howarder3 <howarder3@gmail.com>
2020-06-05 03:58:35 +0800

    date//-03:58:35

--- test.bat
+++ test.bat
@@ -3,7 +3,7 @@ rem ECHO Congratulations! Your first batch file executed successfully.
 
 copy C:\Users\user\.leetcode D:\jupyter_notebook\leetcode_practice\new_practice /Y
 
-for /f "tokens=1-4 delims=-/ " %%i IN ("%date: =0%") DO (
+for /f "tokens=1-4 delims=-/ " %%i IN ("%date") DO (
 set year=%%i
 set month=%%j
 set day=%%k
