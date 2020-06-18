5c40b6f8dda3efa62bf7acab8a27bb63d5b61597
howard <howarder3@gmail.com>
2020-06-18 18:31:13 +0800

    2020/06/18-18:31:13

--- mac_push.sh
+++ mac_push.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+echo "============ Change directory ============"
 cd /Users/howarder3/Documents/GitHub/leetcode_practice 
+
+echo "============ Copying files ============"
 cp -R /Users/howarder3/.leetcode/ /Users/howarder3/Documents/GitHub/leetcode_practice/new_practice 
 
+echo "============ Current datetime ============"
 datetime=$(date '+%Y/%m/%d-%H:%M:%S');
 echo "$datetime"
 
+echo "============ update to GitHub ============"
 git add .
 git commit -m $datetime
 git push origin master
