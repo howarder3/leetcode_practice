807fb32172a2e654cb8ff34ca1d2867b58d01018
howarder3 <howarder3@gmail.com>
2020-06-22 03:11:55 +0800

    2020/06/22-03:11:55

---
+++ windows_push_c++.bat
@@ -0,0 +1,30 @@
+@ECHO OFF
+rem ECHO Congratulations! Your first batch file executed successfully.
+
+copy C:\Users\user\.leetcode_c++ D:\jupyter_notebook\leetcode_practice\new_practice_c++ /Y
+
+for /f "tokens=1-4 delims=-/ " %%i IN ("%date%") DO (
+set year=%%i
+set month=%%j
+set day=%%k
+)
+
+for /f "tokens=1-4 delims=:." %%i IN ("%time: =0%") DO (
+set hour=%%i
+set minute=%%j
+set second=%%k
+rem set centisecond=%%l
+)
+
+SET OutputDir=%year%/%month%/%day%-%hour%:%minute%:%second%
+ECHO ===================================================     
+ECHO Current Time: %OutputDir%, update to github
+ECHO ===================================================     
+
+rem set /p msg="Enter msg: "
+git add .
+rem git commit -m msg
+git commit -m %OutputDir%
+git push origin master
+
+PAUSE
