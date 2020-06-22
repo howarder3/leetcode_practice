eb18a084ac7c1005cd1a7302889bde4a61b645ff
howard <howarder3@gmail.com>
2020-06-22 18:12:33 +0800

    2020/06/22-18:12:33

--- new_practice_c++/1.two-sum.cpp
+++ new_practice_c++/1.two-sum.cpp
@@ -8,7 +8,31 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+
+        vector<int> ans;
+        map<int, int> HashTable;
+        map<int, int>::iterator iter;
+
+        for(int i=0; i<nums.size(); i++){
+            iter = HashTable.find(target - nums[i]);
+
+            // for (map<int, int>::iterator it=HashTable.begin(); it != HashTable.end(); it++)
+            // {
+            //     printf("(%d,%d)\n",iter->first, iter->second);
+            // }
+
+            if(iter != HashTable.end()){
+                // cout << iter->first << iter->second << endl;
+                // printf("(%d,%d)\n",iter->first, iter->second);
+                ans.push_back(iter->second);
+                ans.push_back(i);
+                return ans;
+            }
+            else{
+                HashTable.insert(pair<int,int>(nums[i],i));
+            }
+        }
+        return ans;
     }
 };
 // @lc code=end
